f1497b3679214661ff1c41802e9b9468b9f594ac
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-08-11 17:17:30 +0200

    link opens new tab now

--- src/modules/landing page modules/questions.js
+++ src/modules/landing page modules/questions.js
@@ -10,7 +10,7 @@ function Questions() {
             <footer>
                 <ul className="footer_list">
                     <li><h3>Join the discord</h3></li>
-                    <li><a href=" https://discord.gg/hJ6Kvs7">HERE</a></li>
+                    <li><a href=" https://discord.gg/hJ6Kvs7" target='_blank'>HERE</a></li>
                 </ul>
             </footer>
         </div>
