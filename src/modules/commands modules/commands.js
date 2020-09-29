977a4af38ab89c19ab0035b1f05349e6d6e0aa06
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-09-30 00:06:49 +0200

    added automated command page refreshing

--- src/modules/commands modules/commands.js
+++ src/modules/commands modules/commands.js
@@ -1,78 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
+//import Authentification modules
+import AuthHelperMethods from '../../components/AuthHelperMethods';
 
-function Commands() {
-    return (
-        <div>
-            <div className="placeholder"></div>
-            <div className="center" id="white">
-                <h1>My Commands</h1>
-                <details>
-                    <summary>Moderation</summary>
-                    <ul>
-                        <li>!ban => Bans the person mentioned</li>
-                        <li>!clear => Clears the chat</li>
-                        <li>!close => Clears a voice channel</li>
-                        <li>!kick => Kicks all the members you mention</li>
-                        <li>!lock => Locks a voice channel</li>
-                        <li>!move => Moves all members from one channel to the other</li>
-                        <li>!mute => mutes a person</li>
-                        <li>!purge => Kicks all members who don't have the approved role</li>
-                        <li>!report => reports a member</li>
-                        <li>!role => adds/removes role to all mentioned members</li>
-                    </ul>
-                </details>
-                <div className="placeholder"></div>
-                <details>
-                    <summary>Information</summary>
-                    <ul>
-                        <li>!about => Gives information about the bot and the server</li>
-                        <li>!addrank => Adds a rank to the database</li>
-                        <li>!delrank => Deletes a rank from the database</li>
-                        <li>!help => Returns all commands, or one specific command info</li>
-                        <li>!infractions => Tells you how often you have been reported</li>
-                        <li>!rank => Applies the given rank</li>
-                        <li>!ranks => Shows available ranks</li>
-                        <li>!roster => People who do stuff</li>
-                        <li>!welcome => Give the welcome message</li>
-                        <li>!whois => Returns user information</li>
-                    </ul>
-                </details>
-                <div className="placeholder"></div>
-                <details>
-                    <summary>Fun stuff</summary>
-                    <ul>
-                        <li>!addreddit => Adds a subreddit to be used with !meme</li>
-                        <li>!delreddit => Deletes a subreddit from the database</li>
-                        <li>!edcg => Returns latest community goal details</li>
-                        <li>!edcmdr => Gets CMDR stats from INARA</li>
-                        <li>!edsystem => Displays information about a system</li>
-                        <li>!love => Calculates the love affinity you have for another person</li>
-                        <li>!meme => Sends an epic meme</li>
-                        <li>!reddits => Shows available subreddits</li>
-                        <li>!rps => Rock Paper Scissors game</li>
-                        <li>!urban => Sends an urban dictonary entry</li>
-                    </ul>
-                </details>
-                <div className="placeholder"></div>
-                <details>
-                    <summary>Setup</summary>
-                    <ul>
-                        <li>!setadmin => Set the admin role</li>
-                        <li>!setapproved => Set the approved role</li>
-                        <li>!setautoapproved => Set wether new members get role automatically</li>
-                        <li>!setchannel => Set the greeting channel. If no channel is set I'll use the default one</li>
-                        <li>!setgreeting => Set the server greeting</li>
-                        <li>!setmod => Set the moderator role</li>
-                        <li>!setreports => Set the reports channel</li>
-                        <li>!setserver => Set up the entire server</li>
-                        <li>!setservergreeting => Sets the message that is to be displayed to the rest of the server</li>
-                        <li>!setstartcmd => Set the approving command</li>
-                        <li>!showserver => Shows the server setup result</li>
-                    </ul>
-                </details>
-            </div>
-      </div>  
-    );
+class Commands extends Component {
+
+    Auth = new AuthHelperMethods();
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            moderation: [],
+            info: [],
+            fun: [],
+            setup: []
+        };
+    }
+    componentDidMount() {
+        fetch(process.env.REACT_APP_API_ADDRESS + '/discord/commands/get', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'content-type': 'application/json',
+                'auth-token': this.Auth.getToken()
+            }
+        }).then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        moderation: result.moderation,
+                        info: result.info,
+                        fun: result.fun,
+                        setup: result.setup
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, moderation, info, fun, setup } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <div className="placeholder"></div>
+                    <div className="center" id="white">
+                        <h1>My Commands</h1>
+                        <details>
+                            <summary>Moderation</summary>
+                            <ul>
+                                {moderation.map(item => (
+                                    <li>{item}</li>
+                                ))}
+                            </ul>
+                        </details>
+                        <div className="placeholder"></div>
+                        <details>
+                            <summary>Information</summary>
+                            <ul>
+                                {info.map(item => (
+                                    <li>{item}</li>
+                                ))}
+                            </ul>
+                        </details>
+                        <div className="placeholder"></div>
+                        <details>
+                            <summary>Fun Stuff</summary>
+                            <ul>
+                                {fun.map(item => (
+                                    <li>{item}</li>
+                                ))}
+                            </ul>
+                        </details>
+                        <div className="placeholder"></div>
+                        <details>
+                            <summary>Setup</summary>
+                            <ul>
+                                {setup.map(item => (
+                                    <li>{item}</li>
+                                ))}
+                            </ul>
+                        </details>
+
+
+                    </div>
+                </div>
+            );
+        }
+    }
+
 }
 
 export default Commands;
